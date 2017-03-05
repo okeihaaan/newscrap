dfe548319bcef16408ae951380b8bb0ddb6d39c1
Keiko Okumura (けーちゃん) <7kk0928@gmail.com>
2017-03-05 09:05:43 +0000

    ganbarou

--- app.rb
+++ app.rb
@@ -16,11 +16,13 @@ end
 get '/' do
   @articles = Article.all
   @categories = Category.all
-  unless  session[:user] == nil?
+
+  unless session[:user].nil?
     erb :index
   end
-  
+
   erb :top
+  
 end
 
 
@@ -34,14 +36,23 @@ post '/sign_up' do
     mail: params[:mail],
     password: params[:password],
   })
+  
   redirect '/'
+  
+end
+
+
+get '/sign_in' do
+  erb :sign_in
 end
 
 post '/sign_in' do
-   @user = User.find_by(mail: params[:mail])
-   if @user && @user.authenticate(params[:password])
-     session[:user] = user.id
-     end
+  @user = User.find_by(mail: params[:user])
+    
+  if @user && @user.authenticate(params[:password]) do
+    session[:user] = user.id
+  end
+    
   redirect '/'
 end
 
@@ -50,6 +61,7 @@ get '/sign_out' do
   session[:user] = nil   
   redirect '/'
 end
+
 
 
 get '/scrap' do
@@ -71,7 +83,9 @@ post '/scrap' do
     content: params[:content],
     comment: params[:comment],
     })
+    
   redirect '/'
+  
 end
 
 
@@ -82,46 +96,52 @@ end
 
 
 get '/memo/:id/edit' do
-    @categories = Category.all
-    @articles = Article.find_by({id: params[:id]})
-      if @articles.nil?
-        redirect '/'
-      end
-    erb :edit
+  @categories = Category.all
+  @articles = Article.find_by({id: params[:id]})
+  
+  if @articles.nil?
+    redirect '/'
+  end
+  
+  erb :edit
+  
 end
 
 
 post '/article/:id/update' do
-    @article = Article.find_by({id: params[:id]})
-    if @article.nil?
-        redirect '/'
-    end
-    @article.update({
-        title: params[:title],
-        content: params[:content],
-        souce: params[:souce],
-        url: params[:url],
-        category_id: params[:category],
-        keyword: params[:keyword],
-        url: params[:url],
-        summary1: params[:summary1],
-        summary2: params[:summary2],
-        summary3: params[:summary3],
-        content: params[:content],
-        comment: params[:comment],
-        favorite: params[:favorite],
-    })
-    @article.save
+  @article = Article.find_by({id: params[:id]})
+  
+  if @article.nil?
+      redirect '/'
+  end
+    
+  @article.update({
+    title: params[:title],
+    content: params[:content],
+    souce: params[:souce],
+    url: params[:url],
+    category_id: params[:category],
+    keyword: params[:keyword],
+    url: params[:url],
+    summary1: params[:summary1],
+    summary2: params[:summary2],
+    summary3: params[:summary3],
+    content: params[:content],
+    comment: params[:comment],
+    favorite: params[:favorite],
+  })
+  @article.save
 
-    redirect '/'
+  redirect '/'
 end
 
 
 get '/article/:id/delate' do
-    @article = Article.find_by({id: params[:id]})
-    unless @article.nil?
-        @article.destroy
-    end
+  @article = Article.find_by({id: params[:id]})
+    
+  unless @article.nil?
+    @article.destroy
+  end
 
-    redirect '/'
+  redirect '/'
 end
